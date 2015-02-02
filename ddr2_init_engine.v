11c8d950abd94d2a1ed1f9cdebd5104a65a6a981
C Wang <caowang34@gmail.com>
2015-02-02 13:54:57 -0800

    Update ddr2_init_engine.v

--- ddr2_init_engine.v
+++ ddr2_init_engine.v
@@ -1,3 +1,3 @@
 //Though most of the content was written by myself, 
 //since the first few lines was given by the teaching team, 
-/I still cannot just copy all the content here without their permission.
+//I still cannot just copy all the content here without their permission.
